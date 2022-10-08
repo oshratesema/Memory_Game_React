89bd7f46171b98ced448f80f0789bac66c1f00a6
Oshrat Tesema <oshratesema0@gmail.com>
2022-10-08 17:13:00 +0300

    commit

--- src/conponents/Card.js
+++ src/conponents/Card.js
@@ -1,12 +1,17 @@
 import React from 'react'
 
+function Card({item, id, handleClick}) {
 
-export default function Card({item, id, handleClick}) {
+    const itemClass = item.stat ? " active " + item.stat : ""
 
-    const itemClass = item.state ? "active" + item.state: ""
   return (
-    <div className={'card' + itemClass} onClick={() => handleClick(id)}>
-        <img src={item.img} alt=''/>
-    </div>
-  )
+    <div className={"card" + itemClass} onClick={() => handleClick(id)}>
+    <img src={item.img} alt="" />
+</div>
+)
 }
+
+export default Card
+
+
+
--- src/conponents/Cards.js
+++ src/conponents/Cards.js
@@ -1,58 +1,65 @@
-import React, { useState } from "react";
-import Card from "./Card";
+import React, {useState} from 'react'
+import Card from './Card'
 
 export default function Cards() {
-  const [items, setItems] = useState(
-    [
-      { id: 1, img: "/assets/html.png", state: "" },
-      { id: 1, img: "/assets/html.png", state: "" },
-      { id: 2, img: "/assets/css.png", state: "" },
-      { id: 2, img: "/assets/css.png", state: "" },
-      { id: 3, img: "/assets/js.png", state: "" },
-      { id: 3, img: "/assets/js.png", state: "" },
-      { id: 4, img: "/assets/scss.png", state: "" },
-      { id: 4, img: "/assets/scss.png", state: "" },
-      { id: 5, img: "/assets/react.png", state: "" },
-      { id: 5, img: "/assets/react.png", state: "" },
-      { id: 6, img: "/assets/vue.png", state: "" },
-      { id: 6, img: "/assets/vue.png", state: "" },
-      { id: 7, img: "/assets/angular.png", state: "" },
-      { id: 7, img: "/assets/angular.png", state: "" },
-      { id: 8, img: "/assets/nodejs.png", state: "" },
-      { id: 8, img: "/assets/nodejs.png", state: "" },
-    ].sort(() => Math.random() - 0.5)
-  );
 
-  const [prevClick, setPrevClick] = useState(-1);
+    const [items, setItems] = useState([
+        { id: 1, img: '/assets/html.png', stat: "" },
+        { id: 1, img: '/assets/html.png', stat: "" },
+        { id: 2, img: '/assets/css.png', stat: "" },
+        { id: 2, img: '/assets/css.png', stat: "" },
+        { id: 3, img: '/assets/js.png', stat: "" },
+        { id: 3, img: '/assets/js.png', stat: "" },
+        { id: 4, img: '/assets/scss.png', stat: "" },
+        { id: 4, img: '/assets/scss.png', stat: "" },
+        { id: 5, img: '/assets/react.png', stat: "" },
+        { id: 5, img: '/assets/react.png', stat: "" },
+        { id: 6, img: '/assets/vue.png', stat: "" },
+        { id: 6, img: '/assets/vue.png', stat: "" },
+        { id: 7, img: '/assets/angular.png', stat: "" },
+        { id: 7, img: '/assets/angular.png', stat: "" },
+        { id: 8, img: '/assets/nodejs.png', stat: "" },
+        { id: 8, img: '/assets/nodejs.png', stat: "" }
+    ].sort(() => Math.random() - 0.5))
 
-  const check = () => {
-    if (items[current].id == items[prev].id) {
-      items[current].state = "correct";
-      items[prev].id = "correct";
-      setPrevClick(-1)
-    }else{
+    const [prev, setPrev] = useState(-1)
 
+    const check = (current) => {
+        if(items[current].id == items[prev].id){
+            items[current].stat = "correct"
+            items[prev].stat = "correct"
+            setItems([...items])
+            setPrev(-1)
+        }else{
+            items[current].stat = "wrong"
+            items[prev].stat = "wrong"
+            setItems([...items])
+            setTimeout(() => {
+                items[current].stat = ""
+                items[prev].stat = ""
+                setItems([...items])
+                setPrev(-1)
+            }, 1000)
+        }
     }
-  };
 
-  const handleClick = (id) => {
-    // items[id].state = 'active'
-    // setItems([...items])
-
-    if (prevClick === -1) {
-        items[id].state = 'active'
-        setItems([...items])
-      setPrevClick(id);
-    } else {
-      check(id);
+    const  handleClick = (id) => {
+        if(prev === -1){
+            items[id].stat = "active"
+            setItems([...items])
+            setPrev(id)
+        }else{
+            check(id)
+        }
     }
-  };
+
+
 
   return (
     <div className="container">
-      {items.map((item, index) => (
+    { items.map((item, index) => (
         <Card key={index} item={item} id={index} handleClick={handleClick} />
-      ))}
-    </div>
-  );
+    )) }
+</div>
+)
 }
