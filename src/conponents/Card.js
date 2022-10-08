f4873c24d71e54af11968cbd2aa3697f868bc580
Oshrat Tesema <oshratesema0@gmail.com>
2022-10-08 15:22:26 +0300

    commit

---
+++ src/conponents/Card.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+export default function Card({item}) {
+  return (
+    <div className='card'>
+        <img src={item.img} alt=''/>
+    </div>
+  )
+}
--- src/conponents/Cards.js
+++ src/conponents/Cards.js
@@ -1,26 +1,31 @@
 import React, {useState} from 'react'
+import Card from './Card'
 
 
 export default function Cards() {
     const [items, setItems] = useState ([
-        {id: 1, img:'/img/html.png', state: ''},
-        {id: 1, img:'/img/html.png', state: ''},
-        {id: 2, img:'/img/css.png', state: ''},
-        {id: 2, img:'/img/css.png', state: ''},
-        {id: 3, img:'/img/js.png', state: ''},
-        {id: 3, img:'/img/js.png', state: ''},
-        {id: 4, img:'/img/scss.png', state: ''},
-        {id: 4, img:'/img/scss.png', state: ''},
-        {id: 5, img:'/img/react.png', state: ''},
-        {id: 5, img:'/img/react.png', state: ''},
-        {id: 6, img:'/img/vue.png', state: ''},
-        {id: 6, img:'/img/vue.png', state: ''},
-        {id: 7, img:'/img/angular.svg', state: ''},
-        {id: 7, img:'/img/angular.svg', state: ''},
-        {id: 8, img:'/img/nodejs.svg', state: ''},
-        {id: 8, img:'/img/nodejs.svg', state: ''},
-    ])
+        {id: 1, img:'/assets/html.png', state: ''},
+        {id: 1, img:'/assets/html.png', state: ''},
+        {id: 2, img:'/assets/css.png', state: ''},
+        {id: 2, img:'/assets/css.png', state: ''},
+        {id: 3, img:'/assets/js.png', state: ''},
+        {id: 3, img:'/assets/js.png', state: ''},
+        {id: 4, img:'/assets/scss.png', state: ''},
+        {id: 4, img:'/assets/scss.png', state: ''},
+        {id: 5, img:'/assets/react.png', state: ''},
+        {id: 5, img:'/assets/react.png', state: ''},
+        {id: 6, img:'/assets/vue.png', state: ''},
+        {id: 6, img:'/assets/vue.png', state: ''},
+        {id: 7, img:'/assets/angular.png', state: ''},
+        {id: 7, img:'/assets/angular.png', state: ''},
+        {id: 8, img:'/assets/nodejs.png', state: ''},
+        {id: 8, img:'/assets/nodejs.png', state: ''},
+    ].sort(() => Math.random() - 0.5))
   return (
-    <div>cards</div>
+    <div className='container'>
+        { items.map((item, index) => (
+            <Card key={index} item={item}/>
+        ))}
+    </div>
   )
 }
